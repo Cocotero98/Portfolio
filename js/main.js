6a93410f9b773156cac2f6d2fe9b7adfbe0e4ef9
Cocotero98 <aguilarag98@gmail.com>
2022-07-30 15:32:50 -0300

    Several changes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@ let date = new Date();
 let year = date.getFullYear();
 let copyright = document.querySelector('#copyright');
 copyright.innerHTML = `&copy ${year}`;
+
+
+let toggleMenu = function(id){
+    let navUl = document.querySelector(id);
+    navUl.classList.toggle('open');
+}
+let changeArrow = function(id){
+    let arrow = document.querySelector(id);
+    if(arrow.textContent === 'See more...'){
+        arrow.textContent = 'Hide details';
+    }else{
+        arrow.textContent = 'See more...';
+    }
+}
+
+let navBtn = document.querySelector('#navBtn').addEventListener('click',()=>{toggleMenu('#navMenu')});
+let detailsOne = document.querySelector('#detailsOneBtn').addEventListener('click',()=>{toggleMenu('#detailsOne');changeArrow('#detailsOneBtn')});
+let detailsTwo = document.querySelector('#detailsTwoBtn').addEventListener('click',()=>{toggleMenu('#detailsTwo');changeArrow('#detailsTwoBtn')});
+let detailsThree = document.querySelector('#detailsThreeBtn').addEventListener('click',()=>{toggleMenu('#detailsThree');changeArrow('#detailsThreeBtn')});
+let detailsFour = document.querySelector('#detailsFourBtn').addEventListener('click',()=>{toggleMenu('#detailsFour');changeArrow('#detailsFourBtn')});
+
