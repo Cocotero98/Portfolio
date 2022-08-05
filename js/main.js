38e99f5d55833eb938e630ffade470e9601bdda0
Cocotero98 <aguilarag98@gmail.com>
2022-08-04 21:54:40 -0300

    medium view finished

--- js/main.js
+++ js/main.js
@@ -5,9 +5,14 @@ let copyright = document.querySelector('#copyright');
 copyright.innerHTML = `&copy ${year} Agustin Aguilar`;
 
 //Define functions to display ot hide navMenu or education details
+let navigation = document.querySelector('#navMenu');
+let navToggle = function(){navigation.classList.toggle('open')};
 let toggleMenu = function(id){
     let navUl = document.querySelector(id);
     navUl.classList.toggle('open');
+    if(id==='#navMenu'&window.innerWidth>=560){
+        navUl.classList.remove('open');
+    }
 }
 let changeArrow = function(id){
     let arrow = document.querySelector(id);
@@ -25,10 +30,25 @@ let detailsOne = document.querySelector('#detailsOneBtn').addEventListener('clic
 let detailsTwo = document.querySelector('#detailsTwoBtn').addEventListener('click',()=>{toggleMenu('#detailsTwo');changeArrow('#detailsTwoBtn')});
 let detailsThree = document.querySelector('#detailsThreeBtn').addEventListener('click',()=>{toggleMenu('#detailsThree');changeArrow('#detailsThreeBtn')});
 let detailsFour = document.querySelector('#detailsFourBtn').addEventListener('click',()=>{toggleMenu('#detailsFour');changeArrow('#detailsFourBtn')});
-for(element of navLinks) {
-    console.log(element);
-    element.addEventListener('click',()=>{toggleMenu('#navMenu')})
+// let windows = document.innerWidth;
+// console.log(window.innerWidth);
+let addEventToNavLinks = function(){
+    if (window.innerWidth < 560){
+    for(element of navLinks) {
+    // console.log(element);
+    element.addEventListener('click',navToggle)
 };
+}else if(window.innerWidth >= 560){for(element of navLinks) {
+    // console.log(element);
+    element.removeEventListener('click',navToggle)
+};}
+}
+
+addEventToNavLinks();
+
+window.onresize = addEventToNavLinks;
+
+
 
 let lastUpdated = new Date(document.lastModified);
 let lastYear = lastUpdated.getFullYear();
