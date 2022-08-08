d77afdca55ee254a086d52d46ba0b3307af8dace
Cocotero98 <aguilarag98@gmail.com>
2022-08-08 13:18:00 -0300

    videos only load when scroll down

--- js/main.js
+++ js/main.js
@@ -48,11 +48,70 @@ addEventToNavLinks();
 window.onresize = addEventToNavLinks;
 
 
-
+//Last updated
 let lastUpdated = new Date(document.lastModified);
 let lastYear = lastUpdated.getFullYear();
 let lastMonth = lastUpdated.getMonth();
 let lastDay = lastUpdated.getDay();
 let fullDateUpdated = `${lastMonth}-${lastDay}-${lastYear}`
-
 document.querySelector('#lastUpdated').innerHTML=fullDateUpdated;
+
+
+//Display Youtube videos
+// let displayVideo = function(){
+//     let video=document.createElement('iframe');
+//     video.width = 560;
+//     video.src="https://www.youtube.com/embed/kMG1Lez5ipc";
+//     video.title="Pergamino Chamber of Commerce";
+//     video.frameborder="0";
+//     video.allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+//     video.allowfullscreen=true;
+//     video.autoplay=1;
+//     player.replaceWith(video);
+// }
+
+//width="560" src="https://www.youtube.com/embed/kMG1Lez5ipc" title="Pergamino Chamber of Commerce" 
+// frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+// allowfullscreen
+
+// let player = document.querySelector('.play-video');
+// player.addEventListener('click', displayVideo);
+
+let projectsDivs=document.getElementsByClassName('hidden-project');
+
+let videosToLoad = document.getElementsByClassName('video');
+console.log(videosToLoad)
+let replace = function(item){
+    let content = item.textContent;
+    let video=document.createElement('iframe');
+    video.width = 560;
+    video.src=content;
+    video.title="Pergamino Chamber of Commerce";
+    video.frameborder="0";
+    video.allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    video.allowfullscreen=true;
+    item.replaceWith(video);
+}
+
+const vidOptions={
+    threshold: 0,
+    rootMargin: '0px 0px 400px 0px'
+}
+
+window.onload=()=>{
+    if ('IntersectionObserver' in window){
+        const observer= new IntersectionObserver((items,observer)=>{
+            items.forEach((item)=>{
+                if (item.isIntersecting){
+                    replace(item.target);
+                    console.log(item.target.parentNode);
+                    observer.unobserve(item.target);
+                }
+            })
+        },vidOptions);
+        for(video of videosToLoad){
+            console.log(video.textContent);
+            observer.observe(video)
+        }
+    }
+}
